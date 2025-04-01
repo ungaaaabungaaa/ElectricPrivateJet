45d186f9d23162a7dac363df50954f0db7c685c3
syed <syed@kemuri.in>
2025-04-01 16:57:11 +0530

    fixing unused varaibles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { AuroraText } from "@/components/magicui/aurora-text";
-import { Globe } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center">
       <main>
-        <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl text-white text-center">
-          Coming <AuroraText>Soon</AuroraText>
+        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter  text-white text-center">
+           <AuroraText>Coming Soon</AuroraText>
         </h1>
       </main>
     </div>
